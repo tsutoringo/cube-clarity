9150814054e6e6f9e373290f0baf9779a77eca69
eru-U <180927586+eru-U@users.noreply.github.com>
2025-02-11 08:28:27 +0900

    デスクトップで編集するために一度コミットしました

---
+++ workspaces/cube-clarity-front/src/components/CubeGuide/CubeGuide.tsx
@@ -0,0 +1,14 @@
+import { RubikCube } from "../../lib/rubikcube/RubikCube/RubikCube"
+import { RubikCubeMoveNotation } from "../../lib/rubikcube/RubikCube/MoveNotation"
+import { useState } from "react"
+
+const CubeGuide = (
+  baseCube: RubikCube,
+  cubeList: {
+    move: RubikCubeMoveNotation,
+    cube: RubikCube
+  }[],
+) => {
+  
+}
+
