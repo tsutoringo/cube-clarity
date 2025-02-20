f46c629bb0ea0a3b3f7c0e72d2b2a148a13fc78f
MarshWinds <158841005+MarshWinds@users.noreply.github.com>
2025-02-20 12:29:58 +0900

    app.py : returnの非同期処理のバグの修正 インデントの統一

--- workspaces/cube-clarity-scan-rubik-cube/app.py
+++ workspaces/cube-clarity-scan-rubik-cube/app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import threading
 import subprocess
 from flask import Flask, make_response ,Response
 
@@ -13,36 +14,42 @@ def load_cube_state():
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return {"error": "Cube state not available"}
+    
+def process_call():
+    global current_status
+    current_status = 1
+    command = ["python","main.py"]
+    proc = subprocess.Popen(command)
+    proc.communicate()
+    if proc.poll() != None:
+        current_status = 2
+    return current_status
 
 # ルーティング
 @app.route("/detectface")
 def detect_face():
-  global current_status
-  if current_status == 0:
-   current_status = 1
-   command = ["python","main.py"]
-   proc = subprocess.Popen(command)
-   if proc.poll() == None:
-      return Response(None,status = 423)
-   proc.communicate()
-   if proc.poll() != None:
-      current_status = 2
-  return Response(None,content_type='text/plane',status = 200)
+    global current_status
+    if current_status == 0:
+        call_thread = threading.Thread(target=process_call)
+        call_thread.start()
+    if current_status == 1:
+        return Response(None,status = 423)
+    return Response(None,content_type='text/plane',status = 200)
   
 @app.route("/getface")
 def get_face():
-  while(1):
-    if current_status == 0:
-       return Response(None,status = 404)
-    elif current_status == 1:
-       return Response(None,status = 425)
-    elif current_status == 2:
-       break
-    
-  cube_state = load_cube_state()
-  response = make_response(json.dumps(cube_state, ensure_ascii=False, indent=6))
-  response.headers['Content-Type'] = 'application/json'
-  return response
+    while(1):
+        if current_status == 0:
+            return Response(None,status = 404)
+        elif current_status == 1:
+            return Response(None,status = 425)
+        elif current_status == 2:
+            break
+     
+    cube_state = load_cube_state()
+    response = make_response(json.dumps(cube_state, ensure_ascii=False, indent=6))
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
