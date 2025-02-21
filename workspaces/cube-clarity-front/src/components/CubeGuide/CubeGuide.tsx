1c1317b59898b1dc38ecb0a0c05bcd25958f8efd
eru_U <180927586+eru-U@users.noreply.github.com>
2025-02-21 10:06:37 +0900

    pullするために一旦コミットします

--- workspaces/cube-clarity-front/src/components/CubeGuide/CubeGuide.tsx
+++ workspaces/cube-clarity-front/src/components/CubeGuide/CubeGuide.tsx
@@ -1,9 +1,8 @@
 import { RubikCube } from "../../lib/rubikcube/RubikCube/RubikCube";
 import { RubikCubeMoveNotation } from "../../lib/rubikcube/RubikCube/MoveNotation";
-import { BoxGeometry, OrthographicCamera, Scene, Vector3, WebGLRenderer } from "three";
+import { Vector3, TextureLoader, BoxGeometry, ImageUtils, MeshPhongMaterial, Mesh, PlaneGeometry } from "three";
 import { generateRubikCubeCubeModel } from "../../lib/rubikcube/RubikCubeModel";
 import { useEffect, useRef, useState } from "react";
-import { RubikCubeGroup } from "../../lib/rubikcube/RubikCubeModel"
 import { CubeGuideRenderer } from "./CubeGuideRenderer";
 import { RubikCubeAnimator } from "../../lib/rubikcube/RubikCube/animator/RubikCubeAnimator"
 
@@ -31,32 +30,61 @@ export const CubeGuide = (
 
     // useStateを利用して新しく設定
     setCubeGuideRenderer(cubeGuideRenderer);
+
     // レンダリングする
     cubeGuideRenderer.render();
     
-    const baseCubeGroup = generateRubikCubeCubeModel(baseCube)
-    cubeGuideRenderer.scene.add(baseCubeGroup)
-    
-    for(const currentCube of cubeList){
-      const cubeGroup = generateRubikCubeCubeModel(currentCube.cube)
-      // 次回ここから処理を書きます
-      const cloneCubeGroup = cubeGroup.clone()
+    // 案１
+    // const geometry = new BoxGeometry()
+    // const texture = ImageUtils.loadTexture("images/arrows/leftArrows.svg")
+    // const material = new MeshPhongMaterial({ map:texture });
+    // const cube = new Mesh( geometry, material );
+    // cubeGuideRenderer.scene.add(cube)
 
-      // 回転を書ける
-      const animator = RubikCubeAnimator.generate(cloneCubeGroup,[currentCube.move])
-      // 回転を半分で止める
-      animator.patchProgress(0.5)
+    // 案２
+    const texture = new TextureLoader().load("./images/allows/leftAllows.svg",
+      (tex) => {
+        const w = 5;
+        const h = tex.image.height/(tex.image.width/w)
+
+        const geometry = new PlaneGeometry(1, 1);
+        const material = new MeshPhongMaterial({map:texture});
+        const plane = new Mesh(geometry, material);
+        plane.scale.set(w,h,1);
+        cubeGuideRenderer.scene.add(plane);
+      }
+    )
+
+
+
+
+    // const baseCubeGroup = generateRubikCubeCubeModel(baseCube)
+    // cubeGuideRenderer.scene.add(baseCubeGroup)
+    // =====================================================
+    // for(const currentCube of cubeList){
+    //   const cubeGroup = generateRubikCubeCubeModel(currentCube.cube)
+    //   // 次回ここから処理を書きます
+    //   const cloneCubeGroup = cubeGroup.clone()
       
-      // positionを設定
-      cubeGroup.position.set(0,0,0)
-      cloneCubeGroup.position.set(0,0,0)
+    //   // 回転をかける
+    //   RubikCubeAnimator.generate(cloneCubeGroup,[currentCube.move]).patchProgress(0.5)
+      
+    //   switch(currentCube.move){
+    //     case "R":
+    //   }
+
+    //   // positionを設定
+    //   cubeGroup.position.set(0,0,0)
+    //   cloneCubeGroup.position.set(0,0,0)
       
       
-      // sceneにキューブを追加
-      cubeGuideRenderer.scene.add(cubeGroup)
-      cubeGuideRenderer.scene.add(cloneCubeGroup)
+    //   // sceneにキューブを追加
+    //   cubeGuideRenderer.scene.add(cubeGroup)
+    //   cubeGuideRenderer.scene.add(cloneCubeGroup)
       
-    }
+    // }
+    // =====================================================
+
   }, []);
 
   return (
