bb2eb776a1370e00772c34904c28409dbc84e124
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2025-02-27 00:55:13 +0900

    fmt

--- workspaces/cube-clarity-front/src/components/CubeGuide/CubeGuide.tsx
+++ workspaces/cube-clarity-front/src/components/CubeGuide/CubeGuide.tsx
@@ -1,8 +1,11 @@
-import type { RubikCube } from "../../lib/rubikcube/RubikCube/RubikCube";
-import type { RubikCubeMoveNotation } from "../../lib/rubikcube/RubikCube/MoveNotation";
-import { Canvas, useThree } from "@react-three/fiber";
+import type { RubikCube } from "@lib/rubikcube/RubikCube/RubikCube";
+import type { RubikCubeMoveNotation } from "@lib/rubikcube/RubikCube/MoveNotation";
+import { Canvas } from "@react-three/fiber";
 import { useContext, useEffect, useMemo, useRef } from "react";
-import { RubikCubeThreeGroup, RubikCubeGroupContext } from "@components/RubikCube/RubikCubeGroup";
+import {
+  RubikCubeGroupContext,
+  RubikCubeThreeGroup,
+} from "@components/RubikCube/RubikCubeGroup";
 import {
   Euler,
   MathUtils,
@@ -14,17 +17,16 @@ import {
 import { moveInvert } from "./moveInvert";
 import { movePathCreate } from "./movePathCreate";
 
-
 type MoveGuideElement = {
-  kind: "move",
-  afterCube: RubikCube
-  move: RubikCubeMoveNotation
-}
+  kind: "move";
+  afterCube: RubikCube;
+  move: RubikCubeMoveNotation;
+};
 
 type CubeGuideElement = {
-  kind: "cube",
-  cube: RubikCube
-}
+  kind: "cube";
+  cube: RubikCube;
+};
 
 type GuideElement = MoveGuideElement | CubeGuideElement;
 
@@ -34,156 +36,124 @@ const PADDING = 6;
 export const CubeGuide = ({
   baseCube,
   cubeList,
-}:{
-  baseCube: RubikCube,
+}: {
+  baseCube: RubikCube;
   cubeList: {
     move: RubikCubeMoveNotation;
     cube: RubikCube;
-  }[],
+  }[];
 }) => {
   if (cubeList.length === 0) return;
 
   const canvasElement = useRef<HTMLCanvasElement>(null);
 
   const camera = useMemo(() => {
-    return new OrthographicCamera(
-    );
+    return new OrthographicCamera();
   }, []);
 
   const guides = useMemo(() => {
     const guides: GuideElement[] = [
       {
         kind: "cube",
-        cube: baseCube
-      }
+        cube: baseCube,
+      },
     ];
 
     return cubeList.reduce((acc, current) => {
       acc.push({
         kind: "move",
         afterCube: current.cube,
-        move: current.move
+        move: current.move,
       });
 
       acc.push({
         kind: "cube",
-        cube: current.cube
+        cube: current.cube,
       });
 
       return acc;
     }, guides);
-  }, [baseCube.encodeBase64(), cubeList])
+  }, [baseCube.encodeBase64(), cubeList]);
 
   useEffect(() => {
     if (!canvasElement.current) return;
     const current = canvasElement.current;
-    
+
     camera.left = 0;
     camera.right = current.clientWidth;
     camera.top = 0;
     camera.bottom = current.clientHeight;
-    camera.zoom = 13
+    camera.zoom = 13;
     camera.updateProjectionMatrix();
     camera.position.x = 15;
     camera.position.z = 20;
   }, []);
 
   useEffect(() => {
-    const y = -((Math.ceil(guides.length / 6) * GAP + PADDING) / 2)
+    const y = -((Math.ceil(guides.length / 6) * GAP + PADDING) / 2);
     camera.position.y = y;
   }, [guides.length]);
 
-  const canvasHeight = (Math.ceil(guides.length / 6) * GAP + PADDING) * 12.8
+  const canvasHeight = (Math.ceil(guides.length / 6) * GAP + PADDING) * 12.8;
 
   const rotation = new Euler(
     MathUtils.degToRad(10),
     -MathUtils.degToRad(20),
-    0
+    0,
   );
 
   return (
-    <div style={{
-      height: canvasHeight,
-      width: "100%"
-    }}>
+    <div
+      style={{
+        height: canvasHeight,
+        width: "100%",
+      }}
+    >
       <Canvas
         camera={camera}
         ref={canvasElement}
       >
-        {
-          guides.map((guideElement, index) => {
-            const x = index % 6 * GAP;
-            const y = -(Math.floor(index / 6) * GAP + PADDING);
-            // console.log(x, y)
-            switch (guideElement.kind) {
-              case "cube":
-                return (
-                  <RubikCubeThreeGroup
-                    key={index}
-                    // キューブの表示
-                    rubikCube={guideElement.cube}
-                    position={{
-                      x,
-                      y,
-                      z: 0
-                    }}
-                    rotation={rotation}
-                  />
-                );
-              case "move":
-                return (
-                  <RubikCubeThreeGroup
-                    // ムーブキューブの表示
-                    key={index}
-                    rubikCube={guideElement.afterCube}
-                    animation={{
-                      moves: moveInvert(guideElement.move)?.moves,
-                      progress: moveInvert(guideElement.move)?.progress,
-                    }}
-                    position={{
-                      x,
-                      y,
-                      z: 0,
-                    }}
-                    rotation={rotation}
-                  >
-                    <Arrow move={guideElement.move} />
-                  </RubikCubeThreeGroup>
-                );
-            }
-          })
-        }
-
-        {/* {cubeList.map((item, index) => (
-          <>
-            <RubikCubeThreeGroup
-              // キューブの表示
-              rubikCube={item.cube}
-              position={{
-                x: ,
-                y: ,
-                z: 0
-              }}
-            >
-
-            </RubikCubeThreeGroup>
-            <RubikCubeThreeGroup
-              // ムーブキューブの表示
-              rubikCube={item.cube}
-              animation={{
-                moves: moveInvert(item.move)?.moves,
-                progress: moveInvert(item.move)?.progress,
-              }}
-              position={{
-                x: (index + 1) % 6 * 2 * 2,
-                y: Math.floor(index / 6) * 3,
-                z: 0,
-              }}
-            >
-              <Arrow move={item.move} />
-            </RubikCubeThreeGroup>
-          </>
-        ))} */}
+        {guides.map((guideElement, index) => {
+          const x = index % 6 * GAP;
+          const y = -(Math.floor(index / 6) * GAP + PADDING);
+          // console.log(x, y)
+          switch (guideElement.kind) {
+            case "cube":
+              return (
+                <RubikCubeThreeGroup
+                  key={index}
+                  // キューブの表示
+                  rubikCube={guideElement.cube}
+                  position={{
+                    x,
+                    y,
+                    z: 0,
+                  }}
+                  rotation={rotation}
+                />
+              );
+            case "move":
+              return (
+                <RubikCubeThreeGroup
+                  // ムーブキューブの表示
+                  key={index}
+                  rubikCube={guideElement.afterCube}
+                  animation={{
+                    moves: moveInvert(guideElement.move)?.moves,
+                    progress: moveInvert(guideElement.move)?.progress,
+                  }}
+                  position={{
+                    x,
+                    y,
+                    z: 0,
+                  }}
+                  rotation={rotation}
+                >
+                  <Arrow move={guideElement.move} />
+                </RubikCubeThreeGroup>
+              );
+          }
+        })}
       </Canvas>
     </div>
   );
--- workspaces/cube-clarity-front/src/components/CubeGuide/movePathCreate.ts
+++ workspaces/cube-clarity-front/src/components/CubeGuide/movePathCreate.ts
@@ -7,117 +7,117 @@ import leftArrow from "./images/arrows/left_Arrow.svg";
 import rightArrow from "./images/arrows/right_Arrow.svg";
 import rightCircle from "./images/arrows/right_Circle.svg";
 import leftCircle from "./images/arrows/left_Circle.svg";
-import { RubikCubeMoveNotation } from "@lib/rubikcube/RubikCube/MoveNotation";
-import { Euler, Vector3 } from 'three';
+import type { RubikCubeMoveNotation } from "@lib/rubikcube/RubikCube/MoveNotation";
+import { Euler, Vector3 } from "three";
 
-export const movePathCreate = (move:RubikCubeMoveNotation) => {
+export const movePathCreate = (move: RubikCubeMoveNotation) => {
   let imagePath = "";
   const position = new Vector3();
   const rotation = new Euler();
   if (moveImageList.up.includes(move)) {
     imagePath = upArrow;
-    switch(move){
+    switch (move) {
       case "R":
-        position.set(1,0,1);
-        rotation.set(0,0,0)
+        position.set(1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
       case "R2":
-        position.set(1,0,1);
-        rotation.set(0,0,0)
+        position.set(1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
       case "B":
-        position.set(1,0,-1)
-        rotation.set(0, Math.PI/2, 0)
+        position.set(1, 0, -1);
+        rotation.set(0, Math.PI / 2, 0);
         break;
       case "B2":
-        position.set(1,0,-1)
-        rotation.set(0, Math.PI/2, 0)
+        position.set(1, 0, -1);
+        rotation.set(0, Math.PI / 2, 0);
         break;
       case "L'":
-        position.set(-1,0,1)
-        rotation.set(0,0,0)
+        position.set(-1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
     }
   }
   if (moveImageList.down.includes(move)) {
     imagePath = downArrow;
-    switch(move){
+    switch (move) {
       case "R'":
-        position.set(1,0,1)
-        rotation.set(0,0,0)
+        position.set(1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
       case "B'":
-        position.set(1,0,-1)
-        rotation.set(0, Math.PI/2, 0)
+        position.set(1, 0, -1);
+        rotation.set(0, Math.PI / 2, 0);
         break;
       case "L":
-        position.set(-1,0,1)
-        rotation.set(0,0,0)
+        position.set(-1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
       case "L2":
-        position.set(-1,0,1)
-        rotation.set(0,0,0)
+        position.set(-1, 0, 1);
+        rotation.set(0, 0, 0);
         break;
     }
   }
   if (moveImageList.left.includes(move)) {
     imagePath = leftArrow;
-    switch(move){
+    switch (move) {
       case "U":
-        position.set(0,1,1)
-        rotation.set(0,0,0)
+        position.set(0, 1, 1);
+        rotation.set(0, 0, 0);
         break;
       case "U2":
-        position.set(0,1,1)
-        rotation.set(0,0,0)
+        position.set(0, 1, 1);
+        rotation.set(0, 0, 0);
         break;
       case "D'":
-        position.set(0,-1,1)
-        rotation.set(0,0,0)
+        position.set(0, -1, 1);
+        rotation.set(0, 0, 0);
         break;
     }
   }
   if (moveImageList.right.includes(move)) {
     imagePath = rightArrow;
-    switch(move){
+    switch (move) {
       case "U'":
-        position.set(0,1,1)
-        rotation.set(0,0,0)
+        position.set(0, 1, 1);
+        rotation.set(0, 0, 0);
         break;
       case "D":
-        position.set(0,-1,1)
-        rotation.set(0,0,0)
+        position.set(0, -1, 1);
+        rotation.set(0, 0, 0);
         break;
       case "D2":
-        position.set(0,-1,1)
-        rotation.set(0,0,0)
+        position.set(0, -1, 1);
+        rotation.set(0, 0, 0);
         break;
     }
   }
   if (moveImageList.leftTurn.includes(move)) {
     imagePath = leftCircle;
-    switch(move){
+    switch (move) {
       case "F'":
-        position.set(0,0,1)
+        position.set(0, 0, 1);
         break;
     }
   }
   if (moveImageList.rightTurn.includes(move)) {
     imagePath = rightCircle;
-    switch(move){
+    switch (move) {
       case "F":
-        position.set(0,0,1.51)
-        rotation.set(0,0,0)
+        position.set(0, 0, 1.51);
+        rotation.set(0, 0, 0);
         break;
       case "F2":
-        position.set(0,0,1)
-        rotation.set(0,0,0)
+        position.set(0, 0, 1);
+        rotation.set(0, 0, 0);
         break;
     }
   }
   return {
     path: imagePath,
     position: position,
-    rotation: rotation
+    rotation: rotation,
   };
-}
+};
